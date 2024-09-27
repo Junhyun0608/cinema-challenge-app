f2883fd040b537fe82485a4de8f68e59e98a9fe2
Junhyun0608 <1qweqwe44@gmail.com>
2024-09-27 11:19:18 +0900

    day14: next.js challenge

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
 import MovieItem from "@/components/MovieItem";
 import { MovieData } from "@/types";
+import { delay } from "@/util/delay";
 export default async function Page({
   searchParams,
 }: {
@@ -7,6 +8,7 @@ export default async function Page({
     q?: string;
   };
 }) {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${searchParams.q}`
   );
